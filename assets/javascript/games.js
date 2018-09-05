017a0a0a7dd9ac72f1a0bcbfa49e45d5db8acda5
ShemikaKW <Shemikakw@gmail.com>
2018-09-04 23:21:20 -0400

    Added to word list and images

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,8 +1,9 @@
 // Word List
-var wordList = ["elephant", "giraffe", "tiger", "turtle"];
+var wordList = ["elephant", "giraffe", "tiger", "turtle", "zebra", "lion", "gorilla"];
 
 //Image List
-var wordImage =["assets/images/imageE", "assets/images/imageG.jpg", "assests/images/imageT.jpg", "assets/images/imageTU.jpg"];
+var wordImage =["assets/images/imageE", "assets/images/imageG.jpg", "assests/images/imageT.jpg", "assets/images/imageTU.jpg", "assets/images/imagesZ.jpg",
+"assets/images/lion.jpg", "assets/images/gorilla.jpg"];
 
 // Variables
 
@@ -18,10 +19,12 @@ var letterGuessed = document.getElementById("guessedLetter");
 var won = document.getElementById("win");
 var outputW = document.getElementById("winText");
 var outputL = document.getElementById("loseText");
+// var image = document.getElementById("wordImage");
 
+// Start of Game
 function startGame(){
 
-// Reset
+// Reset for after each word
 rightLetter = [];
 allowedGuesses = 9;
 wrongLetter = [];
@@ -59,11 +62,14 @@ function updateGuessed(letter){
     }
 }
 
+// Win or Lose output
 function winLose(){
 
     if(rightLetter.indexOf(" _ ") === -1){
         won.innerHTML = wins;
         outputW.innerHTML = ("Congratulations You Win");
+    var image = wordList.indexOf(ranWord);
+        document.getElementById("wordImage").src = wordImage[image];
         wins++;
         startGame();
     
@@ -78,7 +84,7 @@ function winLose(){
 
 
 
-// Get user guess
+// User input
 
 document.onkeyup = function(event){
 
